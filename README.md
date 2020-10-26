f821c425fef31aab6761161c20527b457cd73c7f
Sean Thielen <admin@koji-projects.com>
2020-10-26 11:45:18 -0700

    initial

---
+++ README.md
@@ -0,0 +1,64 @@
+# Koji Auth SDK
+
+**User authentication for Koji templates.**
+
+## Overview
+
+The @withkoji/auth package enables you to authenticate users in Koji templates. Today, this is limited to determining the user's role (i.e., whether or not the user is the owner of the Koji) and cannot be used to determine anything else about a user.
+
+## Installation
+
+Install the package in the frontend and backend services of your Koji project.
+
+```
+npm install --save @withkoji/auth
+```
+
+## Basic use
+
+Instantiate `Auth` on the frontend.
+
+```
+import Auth from '@withkoji/auth';
+const auth = new Auth();
+```
+
+Get the user's short-lived callback token and include it with a request to some kind of admin route
+
+```
+auth.getToken((token) => {
+  fetch('/backend/some/admin/route', {
+    method: 'GET',
+    headers: {
+      authorization: token,
+    },
+  });
+});
+
+```
+
+Instantiate `Auth` on the backend and use it to verify the user's role
+```
+import Auth from '@withkoji/auth';
+
+app.get('/backend/some/admin/route', async (req, res) => {
+  const auth = new Auth(
+    process.env.KOJI_PROJECT_ID,
+    process.env.KOJI_PROJECT_TOKEN,
+  );
+
+  const role = await auth.getRole(req.headers.authorization);
+  if (role !== 'admin') {
+    res.sendStatus(401);
+    return;
+  }
+
+  // access granted
+});
+```
+
+## Contributions and questions
+
+See the [contributions page](https://developer.withkoji.com/docs/about/contribute-koji-developers) on the developer site for info on how to make contributions to Koji repositories and developer documentation.
+
+For any questions, reach out to the developer community or the `@Koji Team` on our [Discord server](https://discord.gg/eQuMJF6).
---
+++ src/Auth.ts
@@ -0,0 +1,114 @@
+import fetch from 'node-fetch';
+
+export type UserToken = string;
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+  UNKNOWN = 'unknown',
+}
+
+export default class Auth {
+  private readonly projectId?: string;
+  private readonly projectToken?: string;
+
+  private userToken?: string;
+  private tokenCallbacks: ((userToken: UserToken) => void)[] = [];
+
+  constructor(projectId?: string, projectToken?: string) {
+    this.projectId = projectId;
+    this.projectToken = projectToken;
+
+    // If we're in a frame, register listeners for async callbacks
+    try {
+      if (window && window.parent) {
+        window.addEventListener('message', ({ data }) => {
+          const { event } = data;
+          if (event === 'KojiAuth.TokenCreated') {
+            try {
+              this.userToken = data.token;
+              this.tokenCallbacks.forEach((callback) => {
+                callback(data.userToken);
+              });
+              this.tokenCallbacks = [];
+            } catch (err) {
+              console.log(err);
+            }
+          }
+        });
+      }
+    } catch (err) {}
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Public/frontend methods
+  //////////////////////////////////////////////////////////////////////////////
+
+  // Ask Koji for a token identifying the current user, which can be used to
+  // resolve the user's role
+  public getToken(callback: (userToken: UserToken) => void, forceRefresh: boolean = false) {
+    if (this.userToken && !forceRefresh) {
+      callback(this.userToken);
+      return;
+    }
+
+    this.tokenCallbacks.push(callback);
+
+    try {
+      if (window && window.parent) {
+        window.parent.postMessage({
+          _kojiEventName: '@@koji/auth/getToken',
+        }, '*');
+      }
+    } catch {}
+    return '';
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Backend/validation methods
+  //////////////////////////////////////////////////////////////////////////////
+  public async getRole(userToken: UserToken): Promise<UserRole> {
+    try {
+      const request = await fetch(
+        this.buildUri('/v1/apps/auth/consumer/getRoleForToken'),
+        {
+          method: 'POST',
+          headers: this.getHeaders(userToken) as any,
+        },
+      );
+      const { role } = await request.json();
+      return role;
+    } catch (err) {
+      return UserRole.UNKNOWN;
+    }
+  }
+
+  private getHeaders(userToken?: UserToken): {[index: string]: string|undefined} {
+    const headers: {[index: string]: string|undefined} = {
+      'Content-Type': 'application/json',
+      'X-Koji-Project-Id': this.projectId,
+      'X-Koji-Project-Token': this.projectToken,
+    };
+
+    if (userToken) {
+      headers['X-Koji-Auth-Callback-Token'] = userToken;
+    }
+    return headers;
+  }
+
+  private buildUri(path: string, queryParams: {[index: string]: any} = {}): string {
+    let base: string = `https://rest.api.gokoji.com${path}`;
+
+    if (process.env.NODE_TEST) {
+      base = `http://localhost:3129${path}`;
+    }
+
+    const resolvedParams = Object.keys(queryParams)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`)
+      .join('&');
+    if (resolvedParams) {
+      base = `${base}?${resolvedParams}`;
+    }
+
+    return base;
+  }
+}
